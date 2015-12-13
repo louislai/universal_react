dce740e013f99fed0e2b2375f39eaeaf351473fd
Louis Lai <hoangdung2506@gmail.com>
2015-12-13 19:45:58 +0700

    Add routing

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import http from 'http';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { match, RoutingContext } from 'react-router';
+
+import MainComponent from './components/main';
+import IndexComponent from './components/index';
 
 const routes = {
     path: '',
@@ -19,7 +25,19 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 app.get('*', (req, res) => {
-    res.render('index');
+    match({ routes, location: req.url }, (err, redirectLocation, props) => {
+        if (err) {
+            res.status(500).send(err.message);
+        } else if (redirectLocation) {
+            res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+        } else if (props) {
+            const markup = renderToString(<RoutingContext {...props} />);
+
+            res.render('index', { markup })
+        } else {
+            res.sendStatus(404);
+        }
+    });
 });
 
 const server = http.createServer(app);
--- components/main.js
+++ components/main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Component extends React.Component {
+export default class MainComponent extends React.Component {
     render() {
         return (
             <div>
